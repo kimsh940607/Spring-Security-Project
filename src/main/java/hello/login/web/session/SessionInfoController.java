98c51110c5476e7f902b187c1b2e48791603b706
shkim <shk940607@gmail.com>
2023-09-10 18:10:20 +0900

    Modify SessionController Commit

--- src/main/java/hello/login/web/session/SessionInfoController.java
+++ src/main/java/hello/login/web/session/SessionInfoController.java
@@ -3,7 +3,7 @@ package hello.login.web.session;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
